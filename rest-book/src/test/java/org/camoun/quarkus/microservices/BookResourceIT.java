a94229a27ec8a4ffa284c72293f344682f3f6c05
Camille BERNARD <cabernard@klanik.com>
2024-06-03 20:28:47 +0200

    Init commit

---
+++ rest-book/src/test/java/org/camoun/quarkus/microservices/BookResourceIT.java
@@ -0,0 +1,8 @@
+package org.camoun.quarkus.microservices;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class BookResourceIT extends BookResourceTest {
+    // Execute the same tests but in packaged mode.
+}
---
+++ rest-number/src/test/java/org/camoun/quarkus/microservices/NumberResourceIT.java
@@ -0,0 +1,8 @@
+package org.camoun.quarkus.microservices;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class NumberResourceIT extends NumberResourceTest {
+    // Execute the same tests but in packaged mode.
+}
